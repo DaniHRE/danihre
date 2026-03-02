193e025f5f2b3ba8a24ffea416c0997f6475d9c0
Daniel Henrique <henriqueevaldo@outlook.com>
2026-03-01 22:56:48 -0300

    chore: remove autogenerated content from main.py

--- main.py
+++ main.py
@@ -162,12 +162,6 @@ def main():
     <img alt="GIFOS" src="output.gif">
 </picture>
 
-<sub><i>Generated automatically using [x0rzavi/github-readme-terminal](https://github.com/x0rzavi/github-readme-terminal) on {time_now}</i></sub>
-
-<!-- <details>
-<summary>More details</summary>
-
-</details> -->
 </div>
 
 <!-- Image deletion URL: NONE -->"""
